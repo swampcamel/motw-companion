9655bc9215070bdcb82b77712081c641c32c6b8b
Dan Mace <dmacebeta@gmail.com>
2019-02-19 22:03:47 -0800

    finish outputs

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
       {"type": "claws", "harmValue": 2, "range": "hand"},
       "extra": [{"type": "ADD_RANGE_CLOSE"}]}],
     [{"type": "immortal", "description": "You do not age or sicken, and whenever you suffer harm, you suffer 1-harm less."},
-    {"type": "unholyStrength", "description": "Roll +Weird instead of +Tough when you <strong>kick some ass.</strong>"}],
+    {"type": "unholyStrength", "description": "Roll +Weird instead of +Tough when you kick some ass."}],
     [{"type": "magnum", "harmValue": 3, "range": "close", "reload": true, "volume": "loud", "extraAttribute": null}],
     {"physiology": "man", "aura": "sinister", "clothing": "tailored"},
      [], [], 0, 2)
@@ -45,31 +45,57 @@ class App extends Component {
             )}
           </ul>
         </div>
-        <div>Moves: </div>
+        <div>Moves:
+          {newHero.movesArray.map(( move, index ) =>
+            <div key={index}>
+              <div>{move.type}</div>
+              <div>{move.description}</div>
+            </div>
+          )}
+        </div>
         <div>Stats:
           <ul>
-            <li>Charm</li>
-            <li>Cool</li>
-            <li>Sharp</li>
-            <li>Tough</li>
-            <li>Weird</li>
+            <li>Charm: {newHero.statsObject.charm}</li>
+            <li>Cool: {newHero.statsObject.cool}</li>
+            <li>Sharp: {newHero.statsObject.sharp}</li>
+            <li>Tough: {newHero.statsObject.tough}</li>
+            <li>Weird: {newHero.statsObject.weird}</li>
           </ul>
         </div>
         <div>Luck Meter: {newHero.luckPointValue}</div>
         <div>Harm Meter: {newHero.harmPointValue}</div>
-        <div>Experience Meter{newHero.xpPointValue}</div>
-        <div>Gear: </div>
+        <div>Experience Meter: {newHero.xpPointValue}</div>
+        <div>Gear:
+          {newHero.gearArray.map(( gear, index ) =>
+            <div key={index}>
+              <span><strong>{gear.type} </strong></span>
+              <span>Damage: {gear.harmValue} Harm </span>
+              <span>Range: {gear.range} </span>
+              <span>{gear.reload ? "reload" : null} </span>
+              <span>{gear.volume} </span>
+              <span>{gear.extraAttribute}</span>
+            </div>
+          )}
+        </div>
         <div>Look:
           <ul>
-            <li>Option 1</li>
-            <li>Option 2</li>
-            <li>Option 3</li>
+            <li>Physiology: {newHero.lookObject.physiology}</li>
+            <li>Aura: {newHero.lookObject.aura}</li>
+            <li>Clothing: {newHero.lookObject.clothing}</li>
           </ul>
         </div>
         <div>History: {newHero.history}</div>
         <div>Level: {newHero.level}</div>
-        <div>Improvements: </div>
-        <div>Advanced Improvements: </div>
+        <div>Improvements:
+          {newHero.improvementsArray.map(( improvement, index ) =>
+            <div key={index}>"Improvement"</div>
+          )}
+        </div>
+        <div>Advanced Improvements:
+          {newHero.advancedImprovementsArray.map(( improvement, index ) =>
+            <div key={index}>"Improvement"</div>
+          )}
+        </div>
         </header>
       </div>
     );
