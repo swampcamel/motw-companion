e64800be40fade7dd9fb215cffcd222b6a2f0e3f
Dan Mace <dmacebeta@gmail.com>
2019-04-09 00:23:19 -0700

    add sum data

---
+++ src/constants/KeeperMoves.js
@@ -0,0 +1,198 @@
+export defaunt const keeperMoves = [
+  {
+    id: "km0",
+    title: "Keeper Agenda",
+    content: [
+      {
+        header: "Keeper Agenda",
+        list: [
+          "Make the world seem real",
+          "Play to see what happens",
+          "Make hunters' lives dangerous & scary"
+        ]
+      },
+      {
+        header: "Principles",
+        list: [
+          "Put horror in everyday situations",
+          "Address the hunters, not the players",
+          "Use the Keeper moves, without names",
+          "Be a fan of the hunters",
+          "Build a coherent mythology in play",
+          "Nothing is safe",
+          "Name everyone they meet, make them seem like normal folks",
+          "Ask questions and build on the answers",
+          "Sometimes give them exactly what they earned, rather than all they wanted",
+          "What's happening off-screen?",
+          "Don't always decide what happens",
+          "Everything is a threat"
+        ]
+      },
+      {
+        header: "Always Say...",
+        list: [
+          "What the principles demand",
+          "What the rules demand",
+          "What your preparation demands",
+          "What honesty demands"
+        ]
+      }
+    ]
+  },
+  {
+    id: "km1",
+    title: "Basic Keeper Moves",
+    content: [
+      "Separate them",
+      "Reveal future badness",
+      "Reveal off-screen badness",
+      "Inflict harm, as established",
+      "Make them investigate",
+      "Make them acquire stuff",
+      "Tell the possible consequences and ask if they want to go ahead",
+      "Turn their move back on them",
+      "Offer an opportunity - and maybe a cost"
+      "Take away some of the hunters' stuff",
+      "Put someone in trouble",
+      "Make a threat move, from one of your mystery or arc threats",
+      "After every move, ask what they do next"
+    ]
+  },
+  {
+    id: "km2",
+    title: "Harm Moves",
+    content: {
+      header: "Every time anyone gets hurt, use one of the following",
+      [
+        {
+          title: "0-harm or more",
+          list: [
+            "Momentarily inhibited",
+            "Drop something",
+            "Take -1 forward"
+          ]
+        },
+        {
+          title: "1-harm or more",
+          list: [
+            "Fall down",
+            "Take -1 ongoing",
+            "Pass out",
+            "Intense pain"
+          ]
+        },
+        {
+          title: "Unstable wounds",
+          list: [
+            "+1 harm"
+          ]
+        },
+        {
+          title: "8-harm or more",
+          list: [
+            "Dying or dead"
+          ]
+        }
+      ]}
+    },
+    {
+      id: "km3",
+      title: "Monster Moves",
+      content: [
+        "Hint at its presence",
+        "Display its full might",
+        "Appear suddenly",
+        "Attack with great force and fury",
+        "Seize someone or something",
+        "Attack with stealth and calculation",
+        "Order underlings to do terrible acts",
+        "Destroy something",
+        "Escape, no matter how well contained",
+        "Give chase",
+        "Return to home ground",
+        "Boast and gloat, maybe revealing a secret",
+        "Return from seeming destruction",
+        "Use an unnatural power"
+      ]
+    },
+    {
+      id: "km4",
+      title: "Minion Moves",
+      content: [
+        "A burst of sudden, uncontrolled violence",
+        "Make a coordinated attack",
+        "Capture someone, or steal something",
+        "Reveal a secret",
+        "Deliver someone or something to the master",
+        "Give chase",
+        "Make a threat or demand for the master",
+        "Run away",
+        "Use an unnatural power",
+        "Display a hint of conscience or humanity",
+        "Disobey the master, in some petty way"
+      ]
+    },
+    {
+      id: "km5",
+      title: "Bystander Moves",
+      content: [
+        "Go off alone",
+        "Argue with the hunters",
+        "Get in the way",
+        "Reveal something",
+        "Confess their fears",
+        "Freak out in terror",
+        "Try to help the hunters",
+        "Try to protect people",
+        "Display inability or incompetence",
+        "Seek help or comfort"
+      ]
+    },
+    {
+      id: "km6",
+      title: "Location Moves",
+      content: [
+        "Present a hazard",
+        "Reveal something",
+        "Hide something",
+        "Close a way",
+        "Open a way",
+        "Reshape itself",
+        "Trap someone",
+        "Offer a guide",
+        "Present a guardian",
+        "Something doesn't work properly",
+        "Create a particular feeling"
+      ]
+    },
+    {
+      id: "km7",
+      title: "Hunter Basic Moves",
+      content: [
+        "Act Under Pressure (Cool)",
+        "Help Out (Cool)",
+        "Investigate A Mystery (Sharp)",
+        "Kick Some Ass (Tough)",
+        "Manipulate Someone (Charm)",
+        "Protect Someone (Tough)",
+        "Read A Bad Situation (Sharp)",
+        "Use Magic (Weird)"
+      ]
+    },
+    {
+      id: "km8",
+      title: "End of Session Experience",
+      content: {
+        header: [
+          "1-2 yes &bull; mark 1",
+          "3-4 yes &bull; mark 2"
+        ],
+        list: [
+          "Did they conclude the current mystery?",
+          "Did they save someone from certain death (or worse)?",
+          "Did we learn something new and important about the world?",
+          "Did we learn something new and important about one of the hunters?"
+        ]
+      }
+    }
+  ]
