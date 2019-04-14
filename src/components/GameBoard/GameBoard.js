be4f6404b01287593f40fb3c386656c7d900d01c
Dan Mace <dmacebeta@gmail.com>
2019-04-14 02:49:44 -0700

    Map image layers from database entries to gameboard

--- src/components/GameBoard/GameBoard.js
+++ src/components/GameBoard/GameBoard.js
@@ -1,45 +1,57 @@
 import React from 'react'
-import { Stage, Layer, Rect, Text } from 'react-konva'
+import { Stage, Layer, Rect, Image } from 'react-konva'
 import Konva from 'konva'
+import useImage from 'use-image'
+import { connect } from "react-redux"
+import _ from 'lodash'
+import * as actions from "./../../actions"
 
-export class ColoredRect extends React.Component {
-  state = {
-    color: 'green'
-  };
-  handleClick = () => {
-    this.setState({
-      color: Konva.Util.getRandomColor()
-    });
-  };
-  render() {
-    return (
-      <Rect
-        x={20}
-        y={20}
-        width={50}
-        height={50}
-        fill={this.state.color}
-        shadowBlur={5}
-        onClick={this.handleClick}
-      />
-    );
-  }
+const HeroImage = (props) => {
+  const [image] = useImage(props.image)
+  return (
+    <Image
+      x={20}
+      y={20}
+      width={120}
+      height={120}
+      draggable
+      image={image}
+    />
+  )
 }
 
 class GameBoard extends React.Component {
+  componentWillMount() {
+    this.props.fetchHeroes();
+  }
+
   render() {
+    console.log(this.props.data)
+    const {data} = this.props
+    const imageLoader = _.map(data.heroes, (hero, key) => {
+      return <HeroImage image={hero.imgUrl} key={key}/>
+    })
       // Stage is a div container
     // Layer is actual canvas element (so you may have several canvases in the stage)
     // And then we have canvas shapes inside the Layer
     return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Stage width={window.innerWidth - 80} height={window.innerHeight - 10}>
         <Layer>
-          <Text text="Try click on rect" />
-          <ColoredRect />
+          {imageLoader}
         </Layer>
       </Stage>
     )
   }
 }
 
-export default GameBoard
+const mapStateToProps = state => {
+  let data
+  if(!state.isFetching) {
+    data = state.data
+  }
+  return {
+    data
+  };
+};
+
+export default connect(mapStateToProps, actions)(GameBoard)
