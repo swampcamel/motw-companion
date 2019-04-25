c47e52f41370abb83576483a91d04da9dac27c89
Dan Mace <dmacebeta@gmail.com>
2019-04-25 14:50:45 -0700

    create state for Hero Overlay to allow switching out children components

---
+++ src/components/PartyView/CharacterInfo.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class CharacterInfo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Character Info</div>
+    )
+  }
+}
+
+export default CharacterInfo
--- src/components/PartyView/HeroOverlay.js
+++ src/components/PartyView/HeroOverlay.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import CharacterInfo from './CharacterInfo'
+
 import './HeroOverlay.scss'
 import statsIcon from './../../img/statsIcon.png'
 import historyIcon from './../../img/historyIcon.png'
@@ -9,28 +11,38 @@ import abilitiesIcon from './../../img/abilitiesIcon.png'
 class HeroOverlay extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      selectedMenuItem: 0
+    }
+  }
 
+  handleChangeMenuItem = e => {
+    const menuIndex = parseInt(e.target.id)
+    this.setState({selectedMenuItem: menuIndex})
   }
 
   render() {
+    const menuOptions = [
+      <div>Option 1</div>, <div>Option 2</div>, <div>Option 3</div>, <div>Option 4</div>
+    ]
     return (
       <div className="overlay-container">
         <div className="overlay-toolbar">
           <div className="hero-nav-item">
-            <img src={statsIcon} alt="Hunter Stats"/>
+            <img src={statsIcon} onClick={this.handleChangeMenuItem} alt="Hunter Stats" id="0" />
           </div>
           <div className="hero-nav-item">
-            <img src={historyIcon} alt="Hunter History"/>
+            <img src={historyIcon} onClick={this.handleChangeMenuItem} alt="Hunter History" id="1"/>
           </div>
           <div className="hero-nav-item">
-            <img src={equipmentIcon} alt="Hunter Equipment"/>
+            <img src={equipmentIcon} onClick={this.handleChangeMenuItem} alt="Hunter  Equipment" id="2"/>
           </div>
           <div className="hero-nav-item">
-            <img src={abilitiesIcon} alt="Hunter Abilities"/>
+            <img src={abilitiesIcon} onClick={this.handleChangeMenuItem} alt="Hunter Abilities" id="3"/>
           </div>
         </div>
         <div className="hero-nav-item-view">
-          Hi
+          {menuOptions[this.state.selectedMenuItem]}
         </div>
       </div>
     )
